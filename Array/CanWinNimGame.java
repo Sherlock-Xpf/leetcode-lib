248c1dd72e8ca5c90359302abc3cfe5ea4a96f22
Sherlock-Xpf <sherlock.xpf@gmail.com>
2024-02-20 08:56:24 +0800

    update

--- Array/CanWinNimGame.java
+++ Array/CanWinNimGame.java
@@ -2,4 +2,8 @@ class solution{
     public boolean canWinNim(int n) {
         return n % 4 != 0;
     }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.canWinNim(6));
+    }
 }
